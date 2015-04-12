b12f5720a24cd77963e48d3aeaf2c7e79c8e5a12
epond <epond@users.noreply.github.com>
2015-04-12 08:18:46 +0100

    Tests more readable

--- test/ApplicativesSpec.hs
+++ test/ApplicativesSpec.hs
@@ -6,28 +6,29 @@ import Test.Hspec
 import Applicatives
 import Control.Applicative
 
+success3 :: Validation String Int
+success3 = Success 3
+
 spec :: Spec
 spec = do
     describe "My Validation type" $ do
       it "can be used as a functor" $ do
-        fmap (+1) (Success 3 :: Validation String Int) `shouldBe` Success 4
+        fmap (+1) success3 `shouldBe` Success 4
         fmap (+1) (Failure ["no"] :: Validation String Int) `shouldBe` Failure ["no"]
       it "obeys the functor laws" $ do
         -- TODO use quickcheck
         -- identity law
-        fmap id (Success 3 :: Validation String Int) `shouldBe` Success 3
+        fmap id success3 `shouldBe` Success 3
         fmap id (Failure ["no"] :: Validation String Int) `shouldBe` Failure ["no"]
         -- associativity law
-        let initialVal = (Success 3 :: Validation String Int)
-        fmap ((+1).(*2)) initialVal `shouldBe` fmap (+1) (fmap (*2) initialVal)
+        fmap ((+1).(*2)) success3 `shouldBe` fmap (+1) (fmap (*2) success3)
       it "can be used as an applicative" $ do
-        (+3) <$> (Success 1 :: Validation String Int) `shouldBe` Success 4
-        (*) <$> (Success 2 :: Validation String Int) <*> Success 3 `shouldBe` Success 6
+        (+3) <$> success3 `shouldBe` Success 6
+        (*) <$> success3 <*> success3 `shouldBe` Success 9
         (*) <$> (Failure ["a"] :: Validation String Int) <*> Failure ["b"] `shouldBe` Failure["a", "b"]
       it "obeys the applicative laws" $ do
         -- TODO use quickcheck
-        let initialVal = (Success 3 :: Validation String Int)
-        Success (*2) <*> initialVal `shouldBe` fmap (*2) initialVal
+        Success (*2) <*> success3 `shouldBe` fmap (*2) success3
         -- TODO identity law: pure id <*> v = v
         -- TODO composition law: pure (.) <*> u <*> v <*> w = u <*> (v <*> w)
         -- TODO homomorphism law: pure f <*> pure x = pure (f x)
