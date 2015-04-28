89ef9558f5cd779d1349a3dc1285d3e4e5fa943c
Edward Pond <edward.pond@springer.com>
2015-04-28 09:40:08 +0100

    Comments

--- src/CatTheory/Part04.hs
+++ src/CatTheory/Part04.hs
@@ -3,18 +3,26 @@ module CatTheory.Part04 where
 -- Kleisli Categories
 -- http://bartoszmilewski.com/2014/12/23/kleisli-categories/
 
+-- Construct the Kleisli category for partial functions (define composition and identity)
+
+-- Identity for partial functions
 return :: a -> Maybe a
 return a = Just a
 
+-- Composition for partial functions
 (>=>) :: (a -> Maybe b) -> (b -> Maybe c) -> (a -> Maybe c)
 m1 >=> m2 = \x ->
     case m1 x of Just y -> m2 y
                  Nothing -> Nothing
 
+-- Implement the embellished function safe_reciprocal that returns a valid reciprocal of
+-- its argument, if it’s different from zero
 safe_reciprocal :: Double -> Maybe Double
 safe_reciprocal 0 = Nothing
 safe_reciprocal x = Just $ 1 / x
 
+-- Compose safe_root and safe_reciprocal to implement safe_root_reciprocal that calculates
+-- sqrt(1/x) whenever possible
 safe_root :: Double -> Maybe Double
 safe_root x
     | x >= 0 = Just $ sqrt x
