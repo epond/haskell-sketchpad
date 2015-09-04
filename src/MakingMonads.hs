33b9010f92252e489038383f81e9364a18c1e685
Edward Pond <edward.pond@springer.com>
2015-09-04 14:36:54 +0100

    Add state monad example that compiles under GHC 7.8.4

--- src/MakingMonads.hs
+++ src/MakingMonads.hs
@@ -15,3 +15,14 @@ instance Monad ProbList where
     return x = ProbList [(x,1%1)]  
     m >>= f = flatten (fmap f m)  
     fail _ = ProbList []
+
+
+-- A State s a is a stateful computation that manipulates a state of type s and has a result of type a.
+newtype State s a = State { runState :: s -> (a,s) }
+
+instance Monad (State s) where  
+    return x = State $ \s -> (x,s)
+    -- Notice a pattern: once we have extracted the result from the monadic value, we apply the function f to it to get the new monadic value.
+    (State h) >>= f = State $ \s -> let (a, newState) = h s  
+                                        (State g) = f a  
+                                    in  g newState
--- test/MakingMoadsSpec.hs
+++ test/MakingMoadsSpec.hs
@@ -10,6 +10,11 @@ spec = do
     describe "The ProbList Monad" $ do
         it "calculates the probabilities of three tails in a row with one loaded coin" $ do
             getProbList flipThree `shouldBe` [(False,1 % 40),(False,9 % 40),(False,1 % 40),(False,9 % 40),(False,1 % 40),(False,9 % 40),(False,1 % 40),(True,9 % 40)]
+    describe "The State Monad" $ do
+        it "given an empty stack when pushing 3 then the stack contains 3" $ do
+            runState (push 3) [] `shouldBe` ((), [3])
+        it "given an empty stack when pushing 3 and pop then the result is 3 and the stack is empty" $ do
+            runState push3ThenPop [] `shouldBe` (3, [])
 
 data Coin = Heads | Tails deriving (Show, Eq)  
   
@@ -25,3 +30,17 @@ flipThree = do
     b <- coin  
     c <- loadedCoin  
     return (all (==Tails) [a,b,c])
+
+
+type Stack = [Int]
+
+pop :: State Stack Int  
+pop = State $ \(x:xs) -> (x,xs)  
+  
+push :: Int -> State Stack ()  
+push a = State $ \xs -> ((),a:xs)
+
+push3ThenPop :: State Stack Int
+push3ThenPop = do
+    push 3
+    pop
