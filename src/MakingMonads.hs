316dff577a5c0f234274dee515ab00ac1fa07194
Edward Pond <edward.pond@springer.com>
2015-09-04 15:38:42 +0100

    Remove ProbList

--- src/MakingMonads.hs
+++ src/MakingMonads.hs
@@ -1,24 +1,8 @@
 module MakingMonads where
 
-import Data.Ratio  
 import Control.Applicative (Applicative(..))
 import Control.Monad       (liftM, ap)
   
-newtype ProbList a = ProbList { getProbList :: [(a,Rational)] } deriving Show
-
-instance Functor ProbList where  
-    fmap f (ProbList xs) = ProbList $ map (\(x,p) -> (f x,p)) xs
-
-flatten :: ProbList (ProbList a) -> ProbList a  
-flatten (ProbList xs) = ProbList $ concat $ map multAll xs  
-    where multAll (ProbList innerxs,p) = map (\(x,r) -> (x,p*r)) innerxs
-
-instance Monad ProbList where  
-    return x = ProbList [(x,1%1)]  
-    m >>= f = flatten (fmap f m)  
-    fail _ = ProbList []
-
-
 -- A State s a is a stateful computation that manipulates a state of type s and has a result of type a.
 newtype State s a = State { runState :: s -> (a,s) }
 
@@ -30,12 +14,7 @@ instance Monad (State s) where
                                     in  g newState
 
 
-
 -- The following is required to suppress the warnings generated from the Applicative - Monad Proposal:
-
-instance Applicative ProbList where
-    pure  = return
-    (<*>) = ap
 
 instance Functor (State s) where
     fmap = liftM
--- test/MakingMoadsSpec.hs
+++ test/MakingMoadsSpec.hs
@@ -1,36 +1,15 @@
 module MakingMoadsSpec where
 
 import Test.Hspec
-import Data.Ratio
-import Data.List (all)
 import MakingMonads
 
 spec :: Spec
 spec = do
-    describe "The ProbList Monad" $ do
-        it "calculates the probabilities of three tails in a row with one loaded coin" $ do
-            getProbList flipThree `shouldBe` [(False,1 % 40),(False,9 % 40),(False,1 % 40),(False,9 % 40),(False,1 % 40),(False,9 % 40),(False,1 % 40),(True,9 % 40)]
     describe "The State Monad" $ do
         it "given an empty stack when pushing 3 then the stack contains 3" $ do
             runState (push 3) [] `shouldBe` ((), [3])
         it "given an empty stack when pushing 3 and pop then the result is 3 and the stack is empty" $ do
             runState push3ThenPop [] `shouldBe` (3, [])
-
-data Coin = Heads | Tails deriving (Show, Eq)  
-  
-coin :: ProbList Coin  
-coin = ProbList [(Heads,1%2),(Tails,1%2)]
-
-loadedCoin :: ProbList Coin  
-loadedCoin = ProbList [(Heads,1%10),(Tails,9%10)]
-
-flipThree :: ProbList Bool  
-flipThree = do  
-    a <- coin  
-    b <- coin  
-    c <- loadedCoin  
-    return (all (==Tails) [a,b,c])
-
 
 type Stack = [Int]
 
