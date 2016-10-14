1731d47a8aed33ff691d61c0124ce48907575bb1
Edward Pond <edward.pond@springer.com>
2016-10-14 14:23:32 +0100

    Hole-driven development comments

--- src/HoleDrivenDevelopment.hs
+++ src/HoleDrivenDevelopment.hs
@@ -1,8 +1,20 @@
--- https://www.youtube.com/watch?v=52VsgyexS8Q
+-- Originates from a video by Matthew Brecknell: https://www.youtube.com/watch?v=52VsgyexS8Q
 
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module HoleDrivenDevelopment where
+
+{-  A technique for using types to drive the construction of functional programs.
+    We start with the type signatures of a function but the body is undefined.
+    Pull apart the arguments and give them names, eg. compose has f, g and x.
+    Bind each argument to a wildcard pattern in the scope of a where clause.
+    The ScopedTypeVariables language extension allows the type variables in the function
+    signature to be scoped over the function definition when we use forall in the signature.
+    Provoke a type error by using a noisy hole which tells us the type expected.
+    Use one noisy hole at any given time and silent holes elsewhere so that we don't
+    have to think about which hole caused the type error.
+    Once the function body has been arrived at, remove the where and forall.
+-}
 
 -- A noisy hole that always provokes a type error
 data Hole = Hole
@@ -13,10 +25,11 @@ hole = undefined
 
 -- The forall denotes the scope to which we bind the type variables a, b and c
 compose :: forall a b c. (b -> c) -> (a -> b) -> a -> c
-compose f g x = hole -- Replace this with a noisy hole to begin hole-driven development
+compose f g x = undefined -- Replace this with a noisy hole to begin hole-driven development
     where
         -- This where clause with dummy bindings allows the compiler to check
-        -- type assertions made on the function arguments
+        -- type assertions made on the function arguments.
+        -- You can use a noisy hole here to learn the type of each argument.
         _ = f :: b -> c
         _ = g :: a -> b
         _ = x :: a
