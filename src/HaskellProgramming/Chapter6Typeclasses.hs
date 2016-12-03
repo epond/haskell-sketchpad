b96b316bd1a21f31b1f37993ec712798123ca080
epond <epond@users.noreply.github.com>
2016-12-03 19:01:27 +0000

    Chapter6Typeclasses: TwoIntegers

--- src/HaskellProgramming/Chapter6Typeclasses.hs
+++ src/HaskellProgramming/Chapter6Typeclasses.hs
@@ -1,6 +1,9 @@
 module HaskellProgramming.Chapter6Typeclasses where
 
 data TisAnInteger = TisAn Integer
-
 instance Eq TisAnInteger where
   (==) (TisAn x) (TisAn y) = x == y
+
+data TwoIntegers = Two Integer Integer
+instance Eq TwoIntegers where
+  (==) (Two x x') (Two y y') = x == y && x' == y'
--- test/HaskellProgramming/Chapter6TypeclassesSpec.hs
+++ test/HaskellProgramming/Chapter6TypeclassesSpec.hs
@@ -7,5 +7,9 @@ spec :: Spec
 spec = do
     describe "Typeclasses" $ do
         it "has an Eq instance for TisAnInteger" $ do
-            (TisAn 2) == (TisAn 2) `shouldBe` True
-            (TisAn 2) == (TisAn 3) `shouldBe` False
+            TisAn 2 == TisAn 2 `shouldBe` True
+            TisAn 2 == TisAn 3 `shouldBe` False
+        it "has an Eq instance for TwoIntegers" $ do
+            Two 7 8 == Two 7 8 `shouldBe` True
+            Two 7 8 == Two 7 9 `shouldBe` False
+            Two 7 8 == Two 8 8 `shouldBe` False
