e4e1a6dbcd4c6bfcb741887d4d217f60eff82286
Edward Pond <edward.pond@springer.com>
2017-03-31 14:31:28 +0100

    Chapter10 exercises: filterDbDate

---
+++ src/HaskellProgramming/Chapter10.hs
@@ -0,0 +1,25 @@
+module HaskellProgramming.Chapter10 where
+
+import Data.Time
+
+data DatabaseItem = DbString String
+                  | DbNumber Integer
+                  | DbDate   UTCTime
+                  deriving (Eq, Ord, Show)
+
+theDatabase :: [DatabaseItem]
+theDatabase =
+    [ DbDate (UTCTime
+                (fromGregorian 1911 5 1)
+                (secondsToDiffTime 34123))
+    , DbNumber 9001
+    , DbString "Hello, world!"
+    , DbDate (UTCTime
+                (fromGregorian 1921 5 1)
+                (secondsToDiffTime 34123))
+    ]
+
+filterDbDate :: [DatabaseItem] -> [UTCTime]
+filterDbDate [] = []
+filterDbDate ((DbDate u) : rest) = u : (filterDbDate rest)
+filterDbDate (_ : rest) = filterDbDate rest
---
+++ test/HaskellProgramming/Chapter10Spec.hs
@@ -0,0 +1,11 @@
+module HaskellProgramming.Chapter10Spec where
+
+import Test.Hspec
+import HaskellProgramming.Chapter10
+import Data.Time
+
+spec :: Spec
+spec = do
+    describe "Database Processing" $ do
+        it "has a function that filters for DbDate and returns a list of UTCTime values inside them" $ do
+            filterDbDate theDatabase `shouldBe` [UTCTime (fromGregorian 1911 5 1) (secondsToDiffTime 34123), UTCTime (fromGregorian 1921 5 1) (secondsToDiffTime 34123)]
