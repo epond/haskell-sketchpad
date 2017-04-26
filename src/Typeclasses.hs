244a4c9c7d758efa584e2304f30ebad56167e84d
Edward Pond <edward.pond@springer.com>
2017-04-26 14:25:54 +0100

    Another typeclass example

---
+++ src/Typeclasses.hs
@@ -0,0 +1,16 @@
+module Typeclasses where
+
+class Weighable a where
+    weight :: a -> Int
+
+data Fuel = Petrol | Diesel
+
+data Car = Car Fuel
+data Motorbike = Motorbike
+
+instance Weighable Car where
+    weight (Car Petrol) = 20
+    weight (Car Diesel) = 22
+
+instance Weighable Motorbike where
+    weight _ = 10
---
+++ test/TypeclassesSpec.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module TypeclassesSpec where
+
+import Test.Hspec
+import Typeclasses
+
+spec :: Spec
+spec = do
+    describe "The Weighable typeclass" $ do
+        it "can be used to find the weight of a Petrol Car" $ do
+            weight (Car Petrol) `shouldBe` 20
+        it "can be used to find the weight of a Diesel Car" $ do
+            weight (Car Diesel) `shouldBe` 22
+        it "can be used to find the weight of a Motorbike" $ do
+            weight Motorbike `shouldBe` 10
