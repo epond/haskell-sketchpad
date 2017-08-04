a174a9ba029735a4a4e62b646026a072ca301748
Edward Pond <edward.pond@springer.com>
2017-08-04 15:22:09 +0100

    Chapter 11 exercises: implement Hutton's Razor printExpr

--- src/HaskellProgramming/Chapter11Datatypes.hs
+++ src/HaskellProgramming/Chapter11Datatypes.hs
@@ -155,3 +155,7 @@ data Expr
 eval :: Expr -> Integer
 eval (Lit n) = n
 eval (Add x y) = eval x + eval y
+
+printExpr :: Expr -> String
+printExpr (Lit n) = show n
+printExpr (Add x y) = (printExpr x) ++ " + " ++ (printExpr y)
--- test/HaskellProgramming/Chapter11DatatypesSpec.hs
+++ test/HaskellProgramming/Chapter11DatatypesSpec.hs
@@ -73,3 +73,9 @@ spec = do
     describe "Hutton's Razor" $ do
         it "can reduce an expression to its final sum" $ do
             eval (Add (Lit 1) (Lit 9001)) `shouldBe` 9002
+        it "can print an expression as a String" $ do
+            printExpr (Add (Lit 1) (Lit 9001)) `shouldBe` "1 + 9001"
+            let a1 = Add (Lit 9001) (Lit 1)
+            let a2 = Add a1 (Lit 20001)
+            let a3 = Add (Lit 1) a2
+            printExpr a3 `shouldBe` "1 + 9001 + 1 + 20001"
