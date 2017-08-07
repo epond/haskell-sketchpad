d96098c877507f0a4352baa989895d6cfeae6532
Edward Pond <edward.pond@springer.com>
2017-08-07 13:12:27 +0100

    Chapter 12 exercises: implement replaceThe

--- src/HaskellProgramming/Chapter12Adversity.hs
+++ src/HaskellProgramming/Chapter12Adversity.hs
@@ -1,9 +1,15 @@
 module HaskellProgramming.Chapter12Adversity where
 
+import Data.List (intercalate)
+
 -- String processing
 
 notThe :: String -> Maybe String
-notThe = undefined
+notThe "the" = Nothing
+notThe msg = Just msg
 
 replaceThe :: String -> String
-replaceThe = undefined
+replaceThe = (intercalate " ") . (map swap) . words
+    where swap msg = case (notThe msg) of
+            Just x -> x
+            Nothing -> "a"
