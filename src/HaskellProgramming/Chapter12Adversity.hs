cf7651b6f186dc58a50271856a1569d4387bd215
Edward Pond <edward.pond@springer.com>
2017-09-01 14:25:05 +0100

    Chapter 12 exercises: implement myUnfoldr

--- src/HaskellProgramming/Chapter12Adversity.hs
+++ src/HaskellProgramming/Chapter12Adversity.hs
@@ -131,3 +131,8 @@ eitherMaybe'' f = either' (\_ -> Nothing) (\x -> Just (f x))
 
 myIterate :: (a -> a) -> a -> [a]
 myIterate f x = x:(myIterate f (f x))
+
+myUnfoldr :: (b -> Maybe (a, b)) -> b -> [a]
+myUnfoldr f x = case (f x) of
+    Nothing -> []
+    Just (a, b) -> a:(myUnfoldr f b)
--- test/HaskellProgramming/Chapter12AdversitySpec.hs
+++ test/HaskellProgramming/Chapter12AdversitySpec.hs
@@ -77,3 +77,6 @@ spec = do
     describe "Unfolds" $ do
         it "can iterate" $ do
             (take 5 $ myIterate (+1) 0) `shouldBe` [0,1,2,3,4]
+        it "has a more general anamorphism" $ do
+            (take 5 $ myUnfoldr (\b -> Just (b, b+1)) 0) `shouldBe` [0,1,2,3,4]
+            (take 5 $ myUnfoldr (\b -> Just ((show b), b+1)) 0) `shouldBe` ["0","1","2","3","4"]
